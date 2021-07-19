840cd8969ba2f279571ac19f11a0bda3db4f843f
Tristan Brigham <tristan.brigham@sps.edu>
2021-07-19 22:37:47 +0300

    Updating Finley's Projects

---
+++ Projects/Finley/Class2.py
@@ -0,0 +1,30 @@
+'''
+Hello
+I
+Am
+Finley
+'''
+
+
+FinleysNumber = 1018
+FinleysSecondNumber = -1012
+result = FinleysNumber + FinleysSecondNumber 
+print(result)
+
+#float
+myfloat = 8.67
+print(myfloat)
+
+#charicter
+mycharacter = 'F'
+print(mycharacter)
+
+#string
+mystring = "laksdjalksd asdas dasd asd asd asojioej osiahjoeiq "
+print(mystring)
+
+#array
+arrayOfInts = [6, 7, 3, 7, 4, 7, 5]
+
+for integer in arrayOfInts:
+	print(integer)
