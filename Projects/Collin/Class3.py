d082c96fc83be0f05f01327f6dc56604cab86e05
Tristan Brigham <tristan.brigham@sps.edu>
2021-07-19 22:19:04 +0300

    Updating Collin's code page

---
+++ Projects/Collin/Class3.py
@@ -0,0 +1,11 @@
+#        0    1    2    3   4  5  6   7  8  9
+array = [1 , 10, -1000 -10000, 67, 5, 4, 3, 2]
+ 
+ 
+for thingy in array: 
+     print(thingy)
+
+for lgfcvbhgfdxcvh in range(1093759):
+    print(lgfcvbhgfdxcvh)
+ 
+# lgfcvbhgfdxcvh
