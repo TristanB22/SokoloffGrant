223dfb1c7513bb255d7236fe54f3a74ca54a4610
Tristan Brigham <tristan.brigham@sps.edu>
2021-07-19 22:41:50 +0300

    Updating Iker's repo

---
+++ Projects/Iker/Class1.py
@@ -0,0 +1,30 @@
+'''
+this 
+is a 
+multiline
+comment
+''' 
+
+tristansNumber = 1038
+tristansSecondNumber = -147
+
+result = tristansNumber + tristansSecondNumber
+print(result)
+
+#float
+myfloat = 9.9573
+print(myfloat)
+
+#character
+myCharacter = '='
+print(myCharacter)
+
+#string
+myString = "asdkljaslkdj aslkd jaskldj alskdj alskdj kljriaj sdkl jawij"
+print(myString)
+
+#array
+arrayOfInts = [5, 7, 134, 57, 23, 5357, 68, 0]
+
+for integer in range(16):
+	print(integer)
---
+++ Projects/Iker/ProjectLinks.md
@@ -0,0 +1,5 @@
+# Project Links
+
+### Python Code
+Ikar's code got deleted, but he affirmed to me that everything made sense
+https://ideone.com/UygFbg 
