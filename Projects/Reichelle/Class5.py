eea807b89e315288e22e11ad9f5f0dc74d6a4967
Tristan Brigham <tristan.brigham@sps.edu>
2021-07-23 09:54:36 +0300

    Adding Reichelle's Project

---
+++ Projects/Reichelle/Class5.py
@@ -0,0 +1,21 @@
+#___ is a ___
+class  Animals:
+	def __init__(self, size, color, diet, habitat):
+		#size, color, diet, habitat
+		self.size = size
+		self.color = color
+		self.diet = diet
+		self.habitat = habitat
+ 
+	def getspecies(self):
+		print (self.africanSideneckTurtle)
+	def getsize(self):
+		print(self.size)
+	def getcolor(self):
+		print(self.color)
+	#           size, color,          diet,      habitat
+turtle = Animals(25, "forest green", "snails", "lake")
+ 
+turtle.getturtle()
+turtle.getsize()
+turtle.getcolor()
